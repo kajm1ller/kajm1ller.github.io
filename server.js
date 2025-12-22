136604b569153a9a460fb21fc98da1bbad1d0943
kajm1ller <kajm@bgsu.edu>
2025-12-21 19:58:13 -0500

    yuck

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8000;
+const port = 443;
 
 // Override CSP header
 app.use((req, res, next) => {
