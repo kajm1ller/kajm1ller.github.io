c0595eb34c5a6879c632f2546b60712a08599f72
kajm1ller <kajm@bgsu.edu>
2025-12-21 19:47:15 -0500

    add server script

---
+++ server.js
@@ -0,0 +1,14 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443, '0.0.0.0', () => {
+  console.log('HTTPS Server running on port 443');
+});
