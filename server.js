443fc266f2380668f4dbc25f31a0f98e1235a4d3
kajm1ller <kajm@bgsu.edu>
2025-12-21 19:49:58 -0500

    update server script

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const express = require('express');
 const app = express();
 
 const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/kaj.services/privkey.pem'),
+  cert: fs.readFileSync('//etc/letsencrypt/live/kaj.services/fullchain.pem')
 };
 
 https.createServer(options, app).listen(443, '0.0.0.0', () => {
